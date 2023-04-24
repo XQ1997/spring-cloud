359b20f2ba979fdcbf5e2f7b76f73170cc5eb3fb
xuqiang <2645564458@qq.com>
2023-04-24 15:43:21 +0800

    提高容错

--- spring-cloud/spring-cloud-content/spring-cloud-content-api/src/main/java/com/x/ContentApplication.java
+++ spring-cloud/spring-cloud-content/spring-cloud-content-api/src/main/java/com/x/ContentApplication.java
@@ -2,7 +2,6 @@ package com.x;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author hp
