e3782bf5b55e1cd9f3972e998801cde58ee45026
xuqiang <2645564458@qq.com>
2023-04-27 09:12:27 +0800

    测试接口上传图片成功

--- spring-cloud/spring-cloud-base/src/main/java/com/x/base/exception/GlobalExceptionHandler.java
+++ spring-cloud/spring-cloud-base/src/main/java/com/x/base/exception/GlobalExceptionHandler.java
@@ -25,7 +25,7 @@ import java.util.List;
 public class GlobalExceptionHandler {
 
 	/**此类一场是主动抛出的，可预知异常 HttpStatus.INTERNAL_SERVER_ERROR 返回500*/
-	@ResponseBody
+	/*@ResponseBody
 	@ExceptionHandler(XueChengException.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public RestErrorResponse doXueChengException(XueChengException e) {
@@ -61,7 +61,7 @@ public class GlobalExceptionHandler {
 			errorMsg.append(error.getDefaultMessage()).append("!");
 		});
 		return new RestErrorResponse(errorMsg.toString());
-	}
+	}*/
 
 
 	/*@ResponseBody
--- spring-cloud/spring-cloud-media/spring-cloud-media-api/src/main/java/com/x/media/api/MediaFilesController.java
+++ spring-cloud/spring-cloud-media/spring-cloud-media-api/src/main/java/com/x/media/api/MediaFilesController.java
@@ -37,17 +37,19 @@ public class MediaFilesController {
      }
 
     @ApiOperation("上传文件")
-    @RequestMapping(value = "/upload/coursefile",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @ResponseBody
+    @RequestMapping(value = "/upload/coursefile",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public UploadFileResultDto upload(@RequestPart("filedata") MultipartFile filedata,
                                       @RequestParam(value="folder",required=false) String folder,
                                       @RequestParam(value="objectName",required=false) String objectName) {
-        Long companyId = 1232141425L;
+        System.out.println("测试");
+         Long companyId = 1232141425L;
         UploadFileParamsDto uploadFileParamsDto = new UploadFileParamsDto();
         String contentType = filedata.getContentType();
         uploadFileParamsDto.setContentType(contentType);
         uploadFileParamsDto.setFileSize(filedata.getSize());//文件大小
-        if (contentType.contains("image")) {
+        System.out.println(uploadFileParamsDto);
+        System.out.println(contentType);
+        if (contentType.indexOf("image") >= 0) {
             //是个图片
             uploadFileParamsDto.setFileType("001001");
         } else {
@@ -56,21 +58,11 @@ public class MediaFilesController {
         uploadFileParamsDto.setFilename(filedata.getOriginalFilename());//文件名称
         UploadFileResultDto uploadFileResultDto = null;
         try {
-            uploadFileResultDto = mediaFileService.uploadFile(companyId, filedata.getBytes(), uploadFileParamsDto, folder, objectName);
+            uploadFileResultDto = mediaFileService.uploadFile(companyId, uploadFileParamsDto, filedata.getBytes(), folder, objectName);
         } catch (Exception e) {
             XueChengException.cast("上传文件过程中出错");
         }
 
         return uploadFileResultDto;
-    }
-
-    @ApiOperation("预览文件")
-    @GetMapping("/preview/{mediaId}")
-    public RestResponse<String> getPlayUrlByMediaId(@PathVariable String mediaId){
-
-        //调用service查询文件的url
-
-        MediaFiles mediaFiles = mediaFileService.getFileById(mediaId);
-        return RestResponse.success(mediaFiles.getUrl());
     }
 }
