b044f6fb59411a482eae41af9a61ebd1a6497888
xuqiang <2645564458@qq.com>
2023-05-16 10:34:10 +0800

    增加消息sdk包

---
+++ spring-cloud/spring-cloud-message-sdk/src/main/java/com/x/messagesdk/mapper/MqMessageHistoryMapper.java
@@ -0,0 +1,15 @@
+package com.x.messagesdk.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.x.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface MqMessageHistoryMapper extends BaseMapper<MqMessageHistory> {
+
+}
---
+++ spring-cloud/spring-cloud-message-sdk/src/main/java/com/x/messagesdk/service/MqMessageHistoryService.java
@@ -0,0 +1,16 @@
+package com.x.messagesdk.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.x.messagesdk.model.po.MqMessageHistory;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2022-09-21
+ */
+public interface MqMessageHistoryService extends IService<MqMessageHistory> {
+
+}
