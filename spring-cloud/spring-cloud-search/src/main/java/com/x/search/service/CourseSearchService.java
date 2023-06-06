c365cbb5717cfd4dc9191aa617191c8389a9afac
xuqiang <2645564458@qq.com>
2023-06-06 10:32:37 +0800

    成功部署搜索工程并进行测试

---
+++ spring-cloud/spring-cloud-search/src/main/java/com/x/search/service/CourseSearchService.java
@@ -0,0 +1,27 @@
+package com.x.search.service;
+
+import com.x.base.model.PageParams;
+import com.x.search.dto.SearchCourseParamDto;
+import com.x.search.dto.SearchPageResultDto;
+import com.x.search.po.CourseIndex;
+
+/**
+ * @description 课程搜索service
+ * @author Mr.M
+ * @date 2022/9/24 22:40
+ * @version 1.0
+ */
+public interface CourseSearchService {
+
+
+    /**
+     * @description 搜索课程列表
+     * @param pageParams 分页参数
+     * @param searchCourseParamDto 搜索条件
+     * @return com.xuecheng.base.model.PageResult<com.xuecheng.search.po.CourseIndex> 课程列表
+     * @author Mr.M
+     * @date 2022/9/24 22:45
+    */
+    SearchPageResultDto<CourseIndex> queryCoursePubIndex(PageParams pageParams, SearchCourseParamDto searchCourseParamDto);
+
+ }
