a361779ae1282187db19061901b8f88d8e6afeda
xuqiang <2645564458@qq.com>
2023-06-09 16:07:09 +0800

    初步添加spring Security认证 但配置文件交给nacos管理存在问题

---
+++ spring-cloud/spring-cloud-auth/src/main/java/com/x/ucenter/mapper/XcCompanyUserMapper.java
@@ -0,0 +1,15 @@
+package com.x.ucenter.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.x.ucenter.model.po.XcCompanyUser;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCompanyUserMapper extends BaseMapper<XcCompanyUser> {
+
+}
