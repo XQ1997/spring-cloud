fb3a67a87d2e003f8a4744fbd7cc3b32da1cc54e
xuqiang <2645564458@qq.com>
2023-06-12 11:25:57 +0800

    初步完成校验spring security

--- spring-cloud/spring-cloud-auth/src/main/java/com/x/auth/controller/LoginController.java
+++ spring-cloud/spring-cloud-auth/src/main/java/com/x/auth/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.x.ucenter.mapper.XcUserMapper;
 import com.x.ucenter.model.po.XcUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,12 +22,12 @@ public class LoginController {
     @Autowired
     XcUserMapper userMapper;
 
+
     @RequestMapping("/login-success")
     public String loginSuccess() {
 
         return "登录成功";
     }
-
 
     @RequestMapping("/user/{id}")
     public XcUser getuser(@PathVariable("id") String id) {
@@ -35,15 +36,14 @@ public class LoginController {
     }
 
     @RequestMapping("/r/r1")
+    @PreAuthorize("hasAuthority('p1')")
     public String r1() {
         return "访问r1资源";
     }
 
     @RequestMapping("/r/r2")
+    @PreAuthorize("hasAuthority('p2')")
     public String r2() {
         return "访问r2资源";
     }
-
-
-
 }
