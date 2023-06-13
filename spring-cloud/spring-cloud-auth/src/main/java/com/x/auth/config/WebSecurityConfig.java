63b3a8f405dc28b4a82e29da1624f805d607491f
xuqiang <2645564458@qq.com>
2023-06-13 14:35:51 +0800

    将密码更改为Bcrypt方式加密

--- spring-cloud/spring-cloud-auth/src/main/java/com/x/auth/config/WebSecurityConfig.java
+++ spring-cloud/spring-cloud-auth/src/main/java/com/x/auth/config/WebSecurityConfig.java
@@ -45,8 +45,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
 //        //密码为明文方式
-        return NoOpPasswordEncoder.getInstance();
-//        return new BCryptPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();
+        //密码改为BCrypt
+        return new BCryptPasswordEncoder();
     }
 
     //配置安全拦截机制
@@ -63,5 +64,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+    public static void main(String[] args) {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        /**
+         $2a$10$1sp7I0OdYH3Azs/2lK8YYeuiaGZzOGshGT9j.IYArZftsGNsXqlma
+         $2a$10$m983E2nmJ7ITlesbXzjbzO/M7HL2wP8EgpgX.pPACDm1wG38Lt.na
+         $2a$10$rZvathrW98vVPenLhOnl0OMpUtRTdBkWJ45IkIsTebITS9AFgKqGK
+         $2a$10$2gaMKWCRoKdc42E0jsq7b.munjzOSPOM4yr3GG9M6194E7dOH5LyS
+         $2a$10$I/n93PIKpKL8m4O3AuT5kuZncZhfqV51bfx5sJrplnYoM7FimdboC
+         */
+//        for (int i = 0; i < 5; i++) {
+//            String encode = passwordEncoder.encode("11111");
+//            System.out.println(encode);
+//        }
 
+        boolean matches = passwordEncoder.matches("11111", "$2a$10$m983E2nmJ7ITlesbXzjbzO/M7HL2wP8EgpgX.pPACDm1wG38Lt.na");
+        System.out.println(matches);
+    }
 }
